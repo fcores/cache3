b76d3aae12f9b2d3c33a9c53d55a3e9a8d59dca9
fcoresm <facundo.cores@gmail.com>
2022-07-23 22:38:30 -0300

    Prueba1

---
+++ prueba_cache.py
@@ -0,0 +1,25 @@
+from cachetools import cached,TTLCache
+from time import time
+
+cache = TTLCache(maxsize=100,ttl=86400)
+
+
+inicio_1=time()
+for i in range(0,100000):
+    pass
+fin_1=time()-inicio_1
+
+
+@cached(cache)
+def prueba():
+    for i in range(0,100000):
+        pass
+inicio_2=time()
+prueba()
+fin_2=time()-inicio_2
+
+print(fin_1)
+print(fin_2)
+
+print("FINESSSSSSSkkkSSSSSSS")
+print("Otro")
